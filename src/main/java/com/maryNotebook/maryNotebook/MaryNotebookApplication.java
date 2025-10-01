d1d5e088a5889b12dfe4e29fafce6a36d53c2626
Jackmanek <age002@gmail.com>
2025-10-01 14:17:36 +0200

    mejoras en la seguridad todo

--- src/main/java/com/maryNotebook/maryNotebook/MaryNotebookApplication.java
+++ src/main/java/com/maryNotebook/maryNotebook/MaryNotebookApplication.java
@@ -1,13 +1,34 @@
 package com.maryNotebook.maryNotebook;
 
+import com.maryNotebook.maryNotebook.usuario.entity.Usuario;
+import com.maryNotebook.maryNotebook.usuario.repository.UsuarioRepository;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.xml.crypto.Data;
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class MaryNotebookApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MaryNotebookApplication.class, args);
+	}
+
+	@Bean
+	public ApplicationRunner configure (UsuarioRepository usuarioRepository, PasswordEncoder encoder){
+		return env -> {
+
+			String pass = "quieroser12";
+
+			Usuario u = new Usuario(1L,"Mari","mari@gmail.com",encoder.encode(pass), LocalDateTime.now(), Usuario.Rol.USER);
+
+			//usuarioRepository.save(u);
+
+		};
 	}
 
 }
